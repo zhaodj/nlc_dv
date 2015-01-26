2998280f00393fe3ac8c3a2e9f55f312c187765b
zhaodj <zhaodj1985@gmail.com>
2015-01-26 19:45:01 +0800

    search debug

--- search/search.go
+++ search/search.go
@@ -1,30 +1,33 @@
 package search
 
 import (
-    "strconv"
+	"strconv"
 )
 
 type Boolean int
-const(
-    MUST Boolean = iota
-    SHOULD Boolean = iota
+
+const (
+	MUST   Boolean = iota
+	SHOULD Boolean = iota
 )
 
 var (
-    docCurId = 0
-    termCurId = 0
-    docs = map[int]*Document{}
-    lexicon = map[Term]int{}
-    index = map[int]*IndexItem{}
+	docCurId  = 0
+	termCurId = 0
+	docs      = map[int]*Document{}
+	lexicon   = map[Term]int{}
+	index     = map[int]*IndexItem{}
 )
 
 type Searcher struct {
 	index map[int][]int
 }
 
-type Field interface{
-    IsIndexed()bool
-    Terms()[]Term
+type Field interface {
+	IsIndexed() bool
+	Terms() []Term
+	GetName() string
+	GetValue() interface{}
 }
 
 type BaseField struct {
@@ -32,30 +35,42 @@ type BaseField struct {
 	Name    string
 }
 
-func (f *BaseField)IsIndexed()bool{
-    return f.Indexed
+func (f *BaseField) IsIndexed() bool {
+	return f.Indexed
 }
 
-type IntField struct{
-    BaseField
-    Value int
+func (f *BaseField) GetName() string {
+	return f.Name
 }
 
-func (f *IntField)Terms()[]Term{
-    return []Term{Term{f.Name, strconv.Itoa(f.Value)}}
+type IntField struct {
+	BaseField
+	Value int
 }
 
-type StrSliceField struct{
-    BaseField
-    Value   []string
+func (f *IntField) Terms() []Term {
+	return []Term{Term{f.Name, strconv.Itoa(f.Value)}}
 }
 
-func (f *StrSliceField)Terms()[]Term{
-    res := []Term{}
-    for _,v := range f.Value{
-        res = append(res, Term{f.Name, v})
-    }
-    return res
+func (f *IntField) GetValue() interface{} {
+	return f.Value
+}
+
+type StrSliceField struct {
+	BaseField
+	Value []string
+}
+
+func (f *StrSliceField) Terms() []Term {
+	res := []Term{}
+	for _, v := range f.Value {
+		res = append(res, Term{f.Name, v})
+	}
+	return res
+}
+
+func (f *StrSliceField) GetValue() interface{} {
+	return f.Value
 }
 
 type Document struct {
@@ -68,147 +83,154 @@ type Term struct {
 }
 
 type IndexItem struct {
-    docId int
-    next *IndexItem
+	docId int
+	next  *IndexItem
 }
 
-func (i *IndexItem)add(item *IndexItem)*IndexItem{
-    if i.docId == item.docId{
-        return i
-    }else if i.docId > item.docId{
-        item.next = i
-        i.next = nil
-        return item
-    }else if i.next != nil{
-        i.next = i.next.add(item)
-    }
-    return i
+func (i *IndexItem) add(item *IndexItem) *IndexItem {
+	if i.docId > item.docId {
+		item.next = i
+		i.next = nil
+		return item
+	} else if i.docId < item.docId {
+		if i.next != nil {
+			i.next = i.next.add(item)
+		} else {
+			i.next = item
+		}
+	}
+	return i
 }
 
-func (i *IndexItem)clone()*IndexItem{
-    return &IndexItem{i.docId, i.next}
+func (i *IndexItem) clone() *IndexItem {
+	return &IndexItem{i.docId, i.next}
 }
 
-type Query interface{
-    Match(t *Term) bool
-    Search() *IndexItem
+type Query interface {
+	Match(t *Term) bool
+	Search() *IndexItem
 }
 
-type TermQuery struct{
-    T   *Term
+type TermQuery struct {
+	T *Term
 }
 
-func (q *TermQuery)Match(t *Term)bool{
-    return &t == &q.T
+func (q *TermQuery) Match(t *Term) bool {
+	return &t == &q.T
 }
 
-func (q *TermQuery)Search() *IndexItem{
-    tid, e := lexicon[*q.T]
-    if !e {
-        return nil
-    }
-    ii, e := index[tid]
-    if !e {
-        return nil
-    }
-    return ii
+func (q *TermQuery) Search() *IndexItem {
+	tid, e := lexicon[*q.T]
+	if !e {
+		return nil
+	}
+	ii, e := index[tid]
+	if !e {
+		return nil
+	}
+	return ii
 }
 
-type BooleanQuery struct{
-    Q1  Query
-    Q2  Query
-    Rel Boolean
+type BooleanQuery struct {
+	Q1  Query
+	Q2  Query
+	Rel Boolean
 }
 
-func (q *BooleanQuery)Match(t *Term)bool{
-    res := false
-    switch q.Rel{
-    case MUST:
-        res = q.Q1.Match(t) && q.Q2.Match(t)
-    case SHOULD:
-        res = q.Q1.Match(t) || q.Q2.Match(t)
-    }
-    return res
+func (q *BooleanQuery) Match(t *Term) bool {
+	res := false
+	switch q.Rel {
+	case MUST:
+		res = q.Q1.Match(t) && q.Q2.Match(t)
+	case SHOULD:
+		res = q.Q1.Match(t) || q.Q2.Match(t)
+	}
+	return res
 }
 
-func mergeShould(i1 *IndexItem, i2 *IndexItem) (res *IndexItem){
-    other := i1
-    if i1.docId <= i2.docId{
-        res = i1.clone()
-        other = i2
-    }else{
-        res = i2.clone()
-    }
-    cur := res
-    pre := res
-    for{
-        if cur.docId <= other.docId{
-            if cur.next == nil{
-                if other.next == nil{
-                    break
-                }
-                cur = cur.clone()
-                cur.next = other.next
-                break
-            }else if other.next == nil{
-                break
-            }
-            if cur.docId == other.docId{
-                other = other.next
-            }
-            pre = cur
-            cur = cur.next
-        }else {
-            pre = pre.clone()
-            o := other.clone()
-            pre.next = o
-            o.next = cur
-            if(other.next == nil){
-                break
-            }
-            other = other.next
-        }
-    }
-    return res
+func mergeShould(i1 *IndexItem, i2 *IndexItem) (res *IndexItem) {
+	var other *IndexItem
+	if i1.docId <= i2.docId {
+		res = i1.clone()
+		other = i2
+	} else {
+		res = i2.clone()
+		other = i1.clone()
+	}
+	cur := res
+	pre := res
+	for {
+		if cur.docId < other.docId {
+			if cur.next == nil {
+				if other.next == nil {
+					break
+				}
+				cur = cur.clone()
+				cur.next = other.next
+				break
+			} else if other.next == nil {
+				break
+			}
+			if cur.docId == other.docId {
+				other = other.next
+			}
+			pre = cur
+			cur = cur.next
+		} else if cur.docId == other.docId {
+			other = other.next
+			pre = cur
+			cur = cur.next
+		} else {
+			pre = pre.clone()
+			o := other.clone()
+			pre.next = o
+			o.next = cur
+			if other.next == nil {
+				break
+			}
+			other = other.next
+		}
+	}
+	return res
 }
 
-func mergeMust(i1 *IndexItem, i2 *IndexItem) (res *IndexItem){
-    cur := res
-    ci1, ci2 := i1, i2
-    for{
-        if ci1.docId == ci2.docId{
-            if cur == nil{
-                cur = i1.clone()
-                cur.next = nil
-                res = cur
-            }else{
-                cur.next = i1.clone()
-                cur = cur.next
-                cur.next = nil
-            }
-        }
-        if ci1.next == nil || ci2.next == nil{
-            break
-        }
-        ci1 = ci1.next
-        ci2 = ci2.next
-    }
-    return res
+func mergeMust(i1 *IndexItem, i2 *IndexItem) (res *IndexItem) {
+	cur := res
+	ci1, ci2 := i1, i2
+	for {
+		if ci1.docId == ci2.docId {
+			if cur == nil {
+				cur = i1.clone()
+				cur.next = nil
+				res = cur
+			} else {
+				cur.next = i1.clone()
+				cur = cur.next
+				cur.next = nil
+			}
+		}
+		if ci1.next == nil || ci2.next == nil {
+			break
+		}
+		ci1 = ci1.next
+		ci2 = ci2.next
+	}
+	return res
 }
 
-func (q *BooleanQuery)Search() *IndexItem{
-    ii1 := q.Q1.Search()
-    ii2 := q.Q2.Search()
-    if ii1 == nil {
-        return ii2
-    }
-    if ii2 == nil{
-        return ii1
-    }
-    if q.Rel == MUST{
-        return mergeMust(ii1, ii2)
-    }
-    return mergeShould(ii1, ii2)
+func (q *BooleanQuery) Search() *IndexItem {
+	ii1 := q.Q1.Search()
+	ii2 := q.Q2.Search()
+	if ii1 == nil {
+		return ii2
+	}
+	if ii2 == nil {
+		return ii1
+	}
+	if q.Rel == MUST {
+		return mergeMust(ii1, ii2)
+	}
+	return mergeShould(ii1, ii2)
 }
 
 func NewSearcher() *Searcher {
@@ -241,42 +263,44 @@ func (s *Searcher) Get(term int) ([]int, bool) {
 }
 
 func (s *Searcher) Add(doc *Document) {
-    id := docCurId
-    docs[id] = doc
-    docCurId++
-    ii := &IndexItem{docId:id}
-    for _, f := range doc.Fields {
-        ts := f.Terms()
-        if ts != nil{
-            for _, t := range ts{
-                tid, e := lexicon[t]
-                if !e {
-                    tid = termCurId
-                    termCurId++
-                    lexicon[t] = tid
-                }
-                pre, ex := index[tid]
-                if ex {
-                    pre = pre.add(ii)
-                }
-                index[tid] = pre
-            }
-        }
-    }
+	id := docCurId
+	docs[id] = doc
+	docCurId++
+	for _, f := range doc.Fields {
+		ts := f.Terms()
+		if ts != nil {
+			for _, t := range ts {
+				ii := &IndexItem{docId: id}
+				tid, e := lexicon[t]
+				if !e {
+					tid = termCurId
+					termCurId++
+					lexicon[t] = tid
+				}
+				pre, ex := index[tid]
+				if ex {
+					pre = pre.add(ii)
+				} else {
+					pre = ii
+				}
+				index[tid] = pre
+			}
+		}
+	}
 }
 
 func (s *Searcher) Find(q Query) []*Document {
-    ii := q.Search()
-    if ii == nil{
-	    return nil
-    }
-    res := []*Document{}
-    for{
-        res = append(res, docs[ii.docId])
-        if ii.next == nil{
-            break
-        }
-        ii = ii.next
-    }
-    return res
+	ii := q.Search()
+	if ii == nil {
+		return nil
+	}
+	res := []*Document{}
+	for {
+		res = append(res, docs[ii.docId])
+		if ii.next == nil {
+			break
+		}
+		ii = ii.next
+	}
+	return res
 }
--- search/search_test.go
+++ search/search_test.go
@@ -1,6 +1,7 @@
 package search
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -21,20 +22,45 @@ func TestEqual(t *testing.T) {
 	}
 }
 
-func TestSearch(t *testing.T){
-    idName := "id"
-    termsName := "terms"
-    doc1 := &Document{
-        []Field{
-            &IntField{BaseField{true,idName}, 1},&StrSliceField{BaseField{true,termsName},[]string{"中国","北京"}},
-        },
-    }
-    doc2 := &Document{
-        []Field{
-            &IntField{BaseField{true,idName}, 2},&StrSliceField{BaseField{true,termsName},[]string{"台湾","北京","上海"}},
-        },
-    }
-    searcher := NewSearcher()
-    searcher.Add(doc1)
-    searcher.Add(doc2)
+func TestSearch(t *testing.T) {
+	idName := "id"
+	termsName := "terms"
+	doc1 := &Document{
+		[]Field{
+			&IntField{BaseField{true, idName}, 1}, &StrSliceField{BaseField{true, termsName}, []string{"中国", "北京"}},
+		},
+	}
+	doc2 := &Document{
+		[]Field{
+			&IntField{BaseField{true, idName}, 2}, &StrSliceField{BaseField{true, termsName}, []string{"台湾", "北京", "上海"}},
+		},
+	}
+	searcher := NewSearcher()
+	searcher.Add(doc1)
+	searcher.Add(doc2)
+	q1 := &TermQuery{&Term{termsName, "中国"}}
+	q2 := &TermQuery{&Term{termsName, "北京"}}
+	q3 := &BooleanQuery{q1, q2, SHOULD}
+	q4 := &BooleanQuery{q1, q2, MUST}
+	fmt.Println("search:中国")
+	printDocs(searcher.Find(q1))
+	fmt.Println("search:北京")
+	printDocs(searcher.Find(q2))
+	fmt.Println("search:中国 || 北京")
+	printDocs(searcher.Find(q3))
+	fmt.Println("search:中国 && 北京")
+	printDocs(searcher.Find(q4))
+}
+
+func printDocs(docs []*Document) {
+	for _, d := range docs {
+		printDoc(d)
+	}
+}
+
+func printDoc(doc *Document) {
+	for _, f := range doc.Fields {
+		fmt.Printf("%s:%s ", f.GetName(), f.GetValue())
+	}
+	fmt.Println()
 }
